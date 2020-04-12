f6f91454a4a6d8dbfcec0353cd6ca48247c10dc8
yulong <xiaoyulong001@163.com>
2020-04-13 07:47:56 +0800

    15-数组和对象循环

---
+++ XYMiniLearn/pages/15-数组和对象循环/15.js
@@ -0,0 +1,81 @@
+// pages/demo02/demo02.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: "hello mina",
+    list: [{
+      id: 0,
+      name: "猪八戒"
+    },{
+      id: 1,
+      name: "天蓬元帅"
+    },{
+      id: 2,
+      name: "悟能"
+    }],
+    person: {
+      age: 20,
+      height: 1.88,
+      name: "xiaoli"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
