4074f6fe3f06b7988ccb88a491df8cb26154a541
yulong <xiaoyulong001@163.com>
2020-04-13 20:01:31 +0800

    09-全局配置文件-tabbar字段

---
+++ XYMiniLearn/pages/09-全局配置文件-tabbar字段/category/category.js
@@ -0,0 +1,75 @@
+// pages/img/img.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 1 必须要求当前页面也是 tabBar 页面时，点击tab时触发
+   * 2 点击自己的 tab item 的时候才触发
+   */
+  onTabItemTap: function () {
+    console.log("category onTabItemTap");
+  }
+
+})
