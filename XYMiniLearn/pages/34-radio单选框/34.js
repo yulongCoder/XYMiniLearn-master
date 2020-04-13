d1461b722f987e3d169696f2d0107569c058ad7e
yulong <xiaoyulong001@163.com>
2020-04-13 18:03:16 +0800

    小程序自带组件，swiper navigator，rich-text，button，icon，radio，checkbox

---
+++ XYMiniLearn/pages/34-radio单选框/34.js
@@ -0,0 +1,79 @@
+// pages/34-radio单选框/34.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    gender: "dadaa"
+  },
+
+  handleRadioChange(e) {
+    
+    console.log(e);
+    console.log("-------->");
+    console.log(e.type);
+    console.log(e.detail.value);
+    let gender = e.detail.value;
+
+    this.setData({
+      gender: gender
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
